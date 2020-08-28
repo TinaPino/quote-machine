399667cfb4cbb228c29dc581f4a3da6d31e766a9
TinaPino <70411959+TinaPino@users.noreply.github.com>
2020-08-28 17:05:41 -0600

    Add files via upload

---
+++ quotejs.js
@@ -0,0 +1,194 @@
+let quotopia = [ { 
+quote: "Cause whatever you love can be taken away, so live like it's your dying day.",
+author: "~~~Machine Gun Kelly"
+},{
+quote: "Without order, nothing exists. Without chaos, nothing evolves.",
+author: "~~~Jedi Mind Tricks, Heavy Metal Kings"
+},{
+quote: "Let go, or be dragged.",
+author: "~~~Zen proverb"
+},{
+quote:"You know it's funny when it rains it pours, they got money for wars, but can't feed the poor.",
+author: "~~~2Pac"
+},{
+quote: "We can't change the world until we change ourselves.",
+author: "~~~Biggie Smalls"
+},{
+quote: "When you realize nothing is lacking, the whole world belongs to you.",
+author: "~~~Lao Tzu"
+}, {
+quote: "Mind is like a mad monkey.",
+author: "~~~Sathya Sai Baba"
+}, {
+quote: "Everything that we see is a shadow cast by that which we do not see.",
+author: "~~~Martin Luther King, Jr."
+}, {
+quote: "He who is not bold enough to be stared at from across the abyss is not bold enough to stare into it himself",
+author: "~~~Silent Hill (games)"
+}, {
+quote: "Evil wakes in vengeance. Be careful what you choose.",
+author: "~~~Deborah Kara Unger - Dahlia Gillespie - Silent Hill"
+}, {
+quote: "If you’re scared to take chances, you’ll never have the answers",
+author: "~~~Nas"
+}, {
+quote: "Stop acting so small. You are the universe in ecstatic motion.",
+author: "~~~Rumi"
+}, {
+quote: "No! Try not! Do, or do not. There is no try.",
+author: "~~~Yoda, Star Wars Episode V: The Empire Strikes Back"
+}, {
+quote: "You’re off to Great Places! Today is your day! Your mountain is waiting, So… get on your way",
+author: "~~~Dr. Seuss" 
+}, {
+quote: "The only way to make sense out of change is to plunge into it, move with it, and join the dance",
+author: "~~~Alan Watts"
+}, {
+quote: "Do not let the behavior of others destroy your inner peace.",
+author: "~~~Dalai Lama"
+}, {
+quote: "Nothing ever goes away until it has taught us what we need to know.",
+author: "~~~– Pema Chödrön"
+}, {
+quote: "All that we are, is the result of what we have thought. The mind is everything. What we think, we become.",
+author: "~~~Buddha"
+}, {
+quote: "Move and the way will open.",
+author: "~~~Zen Proverb"
+}, {
+quote: "To a mind that is still, the whole universe surrenders.",
+author: "~~~Chuang Tzu"
+}, {
+quote: "Those who are awake, live in a state of constant amazement.",
+author: "~~~Buddha"
+}, {
+quote: "A black sock is always a clean sock.",
+author: "~~~Anonymous"
+}, {
+quote: "Your anger will get you.",
+author: "~~~Hasana Montano"
+}, {
+quote: "“You can’t wait for inspiration, you have to go after it with a club.”",
+author: "~~~Jack London"
+}, {
+quote: "“Great things are done by a series of small things brought together.”",
+author: "~~~Vincent van Gogh"
+}, {
+quote: "Laugh loudly, laugh often, and most importantly, laugh at yourself.",
+author: "~~~Chelsea Handler"
+}, {
+quote: "Mother Teresa didn’t walk around complaining about her thighs – she had shit to do.",
+author: "~~~Sarah Silverman"
+}, {
+quote: "Every saint got a past; every sinner got a future. Every loser gotta win, and every winner gotta lose someday. ",
+author: "~~~J Cole, Deja Vu (and Oscar Wilde)"
+}, {
+quote: "You have to keep your vision clear, cause only a coward lives in fear.",
+author: "~~~Nas"
+}, {
+quote: "There's nothing wrong with having a tree as a friend.",
+author: "~~~Bob Ross"
+}, {
+quote: "Onion rings are just vegetable donuts.",
+author: "~~~Cookie Monster"
+}, {
+quote: "If you get shown a problem, but have no idea how to control it, then you just decide to get used to the problem.",
+author: "~~~Squeeze (Sorry to Bother You)"
+}, {
+quote: "Mystery creates wonder and wonder is the basis of man’s desire to understand.",
+author: "~~~Neil Armstrong"
+}, {
+quote: "I am always doing that which I cannot do, in order that I may learn how to do it.",
+author: "~~~Pablo Picasso"
+}, {
+quote: "Nothing is impossible, the word itself says ‘I’m possible’!",
+author: "~~~Audrey Hepburn"
+}, {
+quote: "I will not let anyone walk through my mind with their dirty feet.",
+author: "~~~Mahatma Gandhi"
+}, {
+quote: "If things go wrong, don’t go with them.",
+author: "~~~Roger Babson"
+}, {
+quote: "Some people say you are going the wrong way, when it’s simply a way of your own.",
+author: "~~~Angelina Jolie"
+}, {
+quote: "Remember to always be yourself. Unless you suck.",
+author: "~~~Joss Whedon"
+}, {
+quote: "If your heart is broken, make art with the pieces",
+author: "~~~Shane Koyczan"
+}, {
+quote: "Everyone needs to be valued. Everyone has the potential to give something back",
+author: "~~~Princess Diana"
+}, {
+quote: "How wonderful it is that nobody need wait a single moment before starting to improve the world.",
+author: "~~~Anne Frank"
+  }, {
+quote: "Always wake up with a smile knowing that today you are going to have fun accomplishing what others are too afraid to do.",
+author: "~~~Mark Cuban"
+}, {
+quote: "They can't scare me, if I scare them first.",
+author: "~~~Lady Gaga"
+}, {
+quote: "The successful warrior is the average man, with laser-like focus",
+author: "~~~Bruce Lee"
+}, {
+quote: "No.",
+author: "~~~Rosa Parks"
+}, {
+quote: "Truth is like the sun. You can shut it out for a time, but it ain’t goin’ away.",
+author: "~~~Elvis Presley"
+}, {
+quote: "When I was a kid my parents moved a lot, but I always found them.",
+author: "~~~Rodney Dangerfield"
+}, {
+quote: "The truth comes when you can see that your self-image is just a convenient reference point and nothing more, and that you as you had imagined yourself do not exist.",
+author: "~~~Brad Warner, Hardcore Zen: Punk Rock, Monster Movies and the Truth About Reality"
+}, {
+quote: "Hospital food is a funeral of flavor in your mouth.",
+author: "~~~Ali Wong"
+}, {
+quote: "Everyone loves something, even if it's only tortillas.",
+author: "~~~Chogyam Trungpa"
+}, {
+quote: "You know what's crazy? All the people that never dance. I mean, the world is full of music, and they can't hear it, which is sad, because life is short, and we have to dance while we can.",
+author: "~~~Remi Delatour(while on a coke binge), Devious Maids"
+}, {
+quote: "Wounds turn into scars and scars make you tough.",
+author: "~~~ Aisha Tyler"
+}, {
+quote: "This is the life of illusion wrapped up in trouble, laced with confusion. What are we doing here?",
+author: "~~~Grease - written by Barry Gibb and performed by Frankie Valli."
+}, {
+quote: "We may idealize freedom, but when it comes to our habits, we are completely enslaved.",
+author: "~~~Sogyal Rinpoche, The Tibetan Book of Living and Dying"
+}, {
+quote: "If you want to view paradise, simply look around and view it. Anything you want to, do it; want to change the world... there's nothing to it.",
+author: "~~~Willy Wonka"
+}, {
+quote: "If you think anyone is sane you just don't know enough about them.",
+author: "~~~Christopher Moore, Practical Demonkeeping"
+}, {
+quote: "Sometimes you need to scorch everything to the ground, and start over. After the burning the soil is richer, and new things can grow. People are like that, too.",
+author: "~~~Celeste Ng, Little Fires Everywhere"
+} ];
+
+
+window.onload = init; 
+	function init() {
+         generateQuote()
+}
+      
+function generateQuote() {
+      
+let i = Math.floor(Math.random() * quotopia.length);
+let twitterLink ="https://twitter.com/intent/tweet?text="
+let quoteFlipper = quotopia[i].quote.replace(/ /g, "%20");
+twitterLink += quoteFlipper
+let authorFlipper = quotopia[i].author.replace(/ /g, "%20");
+twitterLink += authorFlipper  
+
+document.getElementById("tweet-quote").href = twitterLink;
+document.getElementById("text").innerText = quotopia[i].quote; document.getElementById("author").innerText = quotopia[i].author;
+}
